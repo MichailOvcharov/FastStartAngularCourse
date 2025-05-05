6b6e6638b3352bc9fddc25a6f6e6fa6258f41b0e
Ovcharov Michail <ombp@mail.ru>
2025-05-05 11:36:05 +0500

    Устранение замечаний

--- src/app/modules/courses/directives/change-border-directive.directive.ts
+++ src/app/modules/courses/directives/change-border-directive.directive.ts
@@ -4,7 +4,7 @@ import {Directive, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
   selector: '[appChangeBorderDirective]'
 })
 export class ChangeBorderDirectiveDirective implements OnInit {
-  @Input('appChangeBorderDirective') creationDate: Date | undefined;
+  @Input('appChangeBorderDirective') creationDate!: Date;
   private currentDate = new Date();
 
   constructor(
@@ -14,20 +14,19 @@ export class ChangeBorderDirectiveDirective implements OnInit {
 
   ngOnInit(): void {
     const diffDates = this.differanceDate(this.currentDate, this.creationDate);
-    // @ts-ignore
+
     if (this.creationDate < this.currentDate && diffDates <= 14) {
-    // @ts-ignore
       this.renderer.setStyle(this.element.nativeElement, 'border', '1px solid green');
     }
 
-    // @ts-ignore
+
     if (this.creationDate > this.currentDate) {
       this.renderer.setStyle(this.element.nativeElement, 'border', '1px solid blue');
     }
   }
 
-  differanceDate(firstDate: Date, secondDate: Date | undefined):number {
-   // @ts-ignore
+  differanceDate(firstDate: Date, secondDate: Date):number {
+
     const differance = (firstDate.getTime() - secondDate.getTime()) / (60 * 60 * 24 * 1000);
    return differance >= 0 ? Math.floor(differance) : Math.ceil(differance);
  }
